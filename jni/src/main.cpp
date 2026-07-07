50ffc05b2298a531d11b386cebef2ec884c8a823
DanyaModerov <d.moderov@gmail.com>
2026-07-07 15:22:17 +0300

    upd

--- jni/src/main.cpp
+++ jni/src/main.cpp
@@ -5,6 +5,12 @@
 #include <unistd.h>
 #include <android/log.h>
 #include <imgui.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <signal.h>
+#include <chrono>
+#include <thread>
 
 #define LOG_TAG "ImguiOverlay"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
@@ -14,6 +20,7 @@ bool g_running = true;
 struct sigaction old_sa_int;
 struct sigaction old_sa_term;
 struct sigaction old_sa_quit;
+pid_t game_pid = -1;
 
 void signal_handler(int)
 {
@@ -27,8 +34,66 @@ void cleanup_signals()
     sigaction(SIGQUIT, &old_sa_quit, nullptr);
 }
 
+pid_t get_game_pid()
+{
+    FILE *fp = popen("su -c 'pidof com.axlebolt.standoff2'", "r");
+    if (!fp)
+        return -1;
+    char buf[64];
+    if (fgets(buf, sizeof(buf), fp))
+    {
+        pclose(fp);
+        return atoi(buf);
+    }
+    pclose(fp);
+    return -1;
+}
+
+bool start_game()
+{
+    LOGI("Starting game");
+    int ret = system("su -c 'am start -n com.axlebolt.standoff2/com.google.firebase.MessagingUnityPlayerActivity' 2>/dev/null");
+    if (ret != 0)
+    {
+        LOGE("Failed to start game");
+        return false;
+    }
+    return true;
+}
+
+bool ensure_game_running()
+{
+    game_pid = get_game_pid();
+    if (game_pid > 0)
+    {
+        LOGI("Game already running");
+        return true;
+    }
+    if (!start_game())
+        return false;
+    auto start = std::chrono::steady_clock::now();
+    while (std::chrono::steady_clock::now() - start < std::chrono::seconds(15))
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        game_pid = get_game_pid();
+        if (game_pid > 0)
+        {
+            LOGI("Game started");
+            return true;
+        }
+    }
+    LOGE("Timeout waiting for game process");
+    return false;
+}
+
 int main()
 {
+    if (getuid() != 0)
+    {
+        __android_log_print(ANDROID_LOG_FATAL, LOG_TAG, "Root required");
+        return 1;
+    }
+
     struct sigaction sa{};
     sa.sa_handler = signal_handler;
     sigemptyset(&sa.sa_mask);
@@ -36,6 +101,12 @@ int main()
     sigaction(SIGINT, &sa, &old_sa_int);
     sigaction(SIGTERM, &sa, &old_sa_term);
     sigaction(SIGQUIT, &sa, &old_sa_quit);
+
+    if (!ensure_game_running())
+    {
+        cleanup_signals();
+        return 1;
+    }
 
     android::ANativeWindowCreator::DisplayInfo dispInfo = android::ANativeWindowCreator::GetDisplayInfo();
     if (!initGUI_draw(dispInfo.width, dispInfo.height, false))
@@ -58,6 +129,13 @@ int main()
         if (access("/data/local/tmp/stop_overlay", F_OK) == 0)
         {
             unlink("/data/local/tmp/stop_overlay");
+            g_running = false;
+            break;
+        }
+
+        if (kill(game_pid, 0) != 0)
+        {
+            LOGI("Game process died, exiting");
             g_running = false;
             break;
         }
